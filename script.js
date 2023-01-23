24f31f479abc8cd7283e619dcfdb2a729018cf0c
mesna94 <mesnayusef813@gmail.com>
2023-01-23 10:46:30 +0530

    Fourth commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function buttonClick(val){
+    
+    document.getElementById("display").value+=val
+}
+function clearDisplay(){
+    document.getElementById("display").value=""
+}
+function equalClick(){
+    var text=document.getElementById("display").value
+    var result=eval(text)
+    document.getElementById("display").value=result
+}
